4b796bd9cc4fa16ec5c97a68dd9f21bfc7df46e4
Kolenge <kfonge@gmail.com>
2022-09-15 14:19:22 -0400

    changing coin and adding msgs

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
+confirm("Welcome to Heads or Tails. Best guess 2/3 Wins!")
+prompt("Player 1, It's your turn to guess: HEADS or TAILS?")
+
+
 let coin = document.getElementById("coin")
 let button = document.getElementById("button")
 let heads = 0
 let tails = 0
 
-function coinToss() {
-  let x = Math.floor(Math.random() * 2)
-  if (x === 0) {
+function tossCoin() {
+  let num = Math.floor(Math.random() * 2)
+  if (num === 0) {
     coin.innerHTML =
-      '<img class="heads animate-coin" src="https://lenadesign.org/wp-content/uploads/2020/06/head.png?w=100"/>'
+      '<img class="heads animate-coin" src="https://www.pngegg.com/en/png-tfwff"/>'
     heads += 1
   } else {
     coin.innerHTML =
-      '<img class="tails animate-coin" src="https://lenadesign.org/wp-content/uploads/2020/06/tail.png?w=100"/>'
+      '<img class="tails animate-coin" src="https://www.pngegg.com/en/png-ehiid"/>'
     tails += 1
   }
 }
 
 button.onclick = function () {
-  coinToss()
+  tossCoin()
 }
