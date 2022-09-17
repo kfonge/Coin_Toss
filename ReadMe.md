a749adcf9f02e30211d9bb1a148ad4c7f806ea89
Kolenge <kfonge@gmail.com>
2022-09-17 16:54:35 -0400

    rounds coungint and win and lose state done

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,24 @@
 
+This is a  coin toss game. This is my first attempt at a two player game using elements that I understand.
+
+Your dom project must meet these requirements:
+
+INSTRUCTIONS: Built with HTML, CSS and JavaScript (must be visual and use the DOM not the Console)
+Hosted on Github pages
+
+
+Must be a two player game (either against the computer or against another player)
+
+
+A win state - a way for the player to win the game
+
+High score can be considered a win state
+
+A lose state - a way for the player to lose the game
+
+
+
+A way to keep playing if the game is not over
+
+Multiple rounds to play - a round must begin, end, and there must be a way to check if the game should continue or the overall game is won or lost
+
--- script.js
+++ script.js
@@ -1,25 +1,89 @@
-confirm("Welcome to Heads or Tails. Best guess 2/3 Wins!")
-prompt("Player 1, It's your turn to guess: HEADS or TAILS?")
-
-
 let coin = document.getElementById("coin")
-let button = document.getElementById("button")
-let heads = 0
-let tails = 0
+let tossButton = document.getElementById("tossButton")
+let startButton = document.getElementById("startButton")
+let playerGuess
+let roundsWon = 0
+let wrongGuesses = 0
+//initiate game - upon clicking begin, game instuctions disappear and guess buttons are displayed
 
+startButton.onclick = function () {
+  document.getElementById("startButton").style.display = "none"
+  document.querySelector("#intro").style.display = "none"
+  document.getElementById("callButtons").style.display = "inline-block"
+  // document.getElementById("callTails").style.display = "inline-block";
+}
+
+//once the player makes their call or guess, record it in variable and show the toss coin button
+//
+
+let callHeads = document.getElementById("callHeads")
+let callTails = document.getElementById("callTails")
+
+//when heads button is clicked, the player guess is recorded as heads and both guess buttons disappear
+callHeads.onclick = function () {
+  playerGuess = "HEADS"
+  callButtons.style.display = "none"
+  document.getElementById("tossButton").style.display = "inline-block"
+  console.log("playerGuess is " + playerGuess)
+}
+
+callTails.onclick = function () {
+  playerGuess = "TAILS"
+  callButtons.style.display = "none"
+  document.getElementById("tossButton").style.display = "inline-block"
+  console.log("playerGuess is " + playerGuess)
+}
+
+//finally with the guess recorded, initiate TossTime click event to call toss function
+
+//Define the central toss coin function before its called at 72
 function tossCoin() {
   let num = Math.floor(Math.random() * 2)
-  if (num === 0) {
-    coin.innerHTML =
-      '<img class="heads animate-coin" src="https://www.pngegg.com/en/png-tfwff"/>'
-    heads += 1
-  } else {
-    coin.innerHTML =
-      '<img class="tails animate-coin" src="https://www.pngegg.com/en/png-ehiid"/>'
-    tails += 1
-  }
-}
+  let tossResult
 
-button.onclick = function () {
+  // for (i = 0; i < 2; i++) {
+  if (num === 0) {
+    //toss result is heads, display the heads coin image
+    tossResult = "HEADS"
+    coin.innerHTML =
+      '<img class="heads animate-coin" src="https://lenadesign.org/wp-content/uploads/2020/06/head.png?w=100"/>'
+  } else {
+    //toss result is tails, display tails image
+    tossResult = "TAILS"
+    coin.innerHTML =
+      '<img class="tails animate-coin" src="https://lenadesign.org/wp-content/uploads/2020/06/tail.png?w=100"/>'
+  }
+  //Check player's guess against outcome and Notify player
+  if (tossResult == playerGuess) {
+    setTimeout(() => {
+      alert("Great guesswork, you won this round! Toss Again")
+    }, 2000)
+    roundsWon++
+    console.log("rounds won:" + roundsWon)
+    //display the guess/call buttons and TossTime Again
+  } else {
+    setTimeout(() => {
+      alert("Sorry! Your guess was incorrect. Try again")
+    }, 2000)
+    wrongGuesses++
+    console.log("rounds lost:" + wrongGuesses)
+  }
+  console.log("playerGuess is " + playerGuess)
+  console.log("toss result is " + tossResult)
+}
+// 
+tossButton.onclick = function () {
   tossCoin()
 }
+
+// win state
+
+if (roundsWon >= 2) alert("Congratulations COIN MASTER - you've WON the game")
+//lose state
+
+if (wrongGuessTally > 1)
+  alert(
+    "GAME OVER - You made too many wrong guesses and lost. Reload the page to reset and play again."
+  )
+document.getElementById("tossButton").style.display = none
+document.getElementsByTagName("h1").textcontent = "GAME OVER"
